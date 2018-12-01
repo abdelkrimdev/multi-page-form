8f8dde3b8764632a6f8a995ef1efea033aef7488
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-12-01 11:39:24 +0100

    setup redux store

---
+++ src/configs/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import loggerMiddleware from 'redux-logger'
+
+const store = createStore(
+    combineReducers({
+        
+    }),
+    applyMiddleware(loggerMiddleware)
+)
+
+export default store

d5e0be9d58e7e1785e1b5397840f8b57cfade8c7
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-12-01 13:59:57 +0100

    fix lint

--- src/configs/store.js
+++ src/configs/store.js
@@ -2,10 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import loggerMiddleware from 'redux-logger'
 
 const store = createStore(
-    combineReducers({
-        
-    }),
-    applyMiddleware(loggerMiddleware)
+  combineReducers({
+
+  }),
+  applyMiddleware(loggerMiddleware)
 )
 
 export default store

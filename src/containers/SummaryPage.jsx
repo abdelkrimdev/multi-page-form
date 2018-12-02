54efee9b4721f7ffb890994c4d266da956814588
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-12-02 23:09:19 +0100

    implement summary page

--- src/containers/SummaryPage.jsx
+++ src/containers/SummaryPage.jsx
@@ -1,27 +1,61 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export class SummaryPage extends Component {
   static propTypes = {
-      
+    form: PropTypes.object
   }
 
-  render() {
+  render () {
+    const { step, fullName, emailAddress, phoneNumber, salaryIndication } = this.props.form
+
     return (
-      <div>
-        SummaryPage!!
-      </div>
+      <React.Fragment>
+        { step === 0
+          ? <div className='not-registered'>
+            Please click <Link to='/registration'>here</Link> to register yourself.
+          </div>
+          : <React.Fragment>
+            <div className='summary-title'>
+              Confirm your Details
+            </div>
+            <div className='summary-content'>
+              { fullName &&
+                <div>
+                  <span className='key-item'>Full Name:</span>
+                  <span className='value-item'> {fullName}</span>
+                </div>
+              }
+              { emailAddress &&
+                <div>
+                  <span className='key-item'>Email Address:</span>
+                  <span className='value-item'> {emailAddress}</span>
+                </div>
+              }
+              { phoneNumber &&
+                <div>
+                  <span className='key-item'>Phone Number:</span>
+                  <span className='value-item'> {phoneNumber}</span>
+                </div>
+              }
+              { salaryIndication &&
+                <div>
+                  <span className='key-item'>Salary Indication:</span>
+                  <span className='value-item'> {salaryIndication}</span>
+                </div>
+              }
+            </div>
+          </React.Fragment>
+        }
+      </React.Fragment>
     )
   }
 }
 
-const mapStateToProps = (state) => ({
-  
-})
-
-const mapDispatchToProps = {
-  
+const mapStateToProps = (state) => {
+  return { form: state.form }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SummaryPage)
+export default connect(mapStateToProps)(SummaryPage)
--- src/containers/SummaryPage.spec.jsx
+++ src/containers/SummaryPage.spec.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
 
-import { SummaryPage } from './SummaryPage'
+import ConnectedSummaryPage, { SummaryPage } from './SummaryPage'
+
+const mockStore = configureStore()
 
 describe('summary page', () => {
   it('should render without crashing.', () => {
     const wrapper = shallow(
-      <SummaryPage />
+      <SummaryPage form={{ step: 0 }} />
     )
 
     expect(wrapper).toBeDefined()
+  })
+
+  it('should render correctly.', () => {
+    const wrapper = shallow(<SummaryPage form={{ step: 0 }} />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should connect to the store without crashing.', () => {
+    const store = mockStore({
+      form: { step: 0 }
+    })
+
+    const container = shallow(<ConnectedSummaryPage store={store} />).dive()
+
+    expect(container).toBeDefined()
   })
 })
